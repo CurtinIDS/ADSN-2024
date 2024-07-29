83028f47a53dba4e2b01df76dbce3b384ca86f1e
Dario Limongi <dario.limongi@curtin.edu.au>
2024-07-29 10:15:53 +0800

    Fix missing images with unoptimized

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // enable static page exports, required for publishing a Next.js project to GitHub Pages
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
