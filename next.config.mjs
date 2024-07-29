d8b42c773d8353be2833536886af29361c1a9dc1
Dario Limongi <dario.limongi@curtin.edu.au>
2024-07-29 10:07:10 +0800

    Remove basePath, GitHub Action should sort it out automatically

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/asdn24-web", // append GitHub Pages 'subpath' for resolving image URLs
   output: "export", // enable static page exports, required for publishing a Next.js project to GitHub Pages
 };
 
