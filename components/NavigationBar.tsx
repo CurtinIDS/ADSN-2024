6e81644604afa5227511fde5874389fd91386206
kathrynnapier <42160715+kathrynnapier@users.noreply.github.com>
2024-09-16 11:01:12 +0800

    Revert "Implemented accommodation page"

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -53,7 +53,7 @@ const NavigationBar = () => {
       subtabs: [
         { name: "Registration", link: "https://www.eventbrite.com.au/e/australian-data-science-network-conference-2024-tickets-967171274317" },
         { name: "Locations", link: "/ADSN-2024/locations" },
-        { name: "Accommodation", link: "/ADSN-2024/accommodation" },
+        { name: "Accommodation", link: "/ADSN-2024/comingsoon" },
       ],
     },
     {
