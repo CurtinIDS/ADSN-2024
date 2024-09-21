3cabc6d1a85410cd2ae60204d25b8ee581fd3c6a
David Curtin <291928k@curtin.edu.au>
2024-09-21 18:12:43 +0800

    pin parking location on the first map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 
-interface MapProps {
-  // https://developers.google.com/maps/documentation/places/web-service/place-id
-  place_id: string;
-  location: string;
+interface Location {
+  name: string;
+  lat: number;
+  lng: number;
 }
 
-const Map: React.FC<MapProps> = ({ location, place_id }) => {
+interface MapProps {
+  locations: Location[];
+  center: { lat: number; lng: number };
+}
+
+const Map: React.FC<MapProps> = ({ locations, center }) => {
   const api_key = process.env.GOOGLE_MAPS_API_KEY;
+  const q = locations.map(loc => `${loc.name}@${loc.lat},${loc.lng}`).join('|');
+  const centerStr = `${center.lat},${center.lng}`;
 
   return (
     <div>
       <iframe
-        title={`Map of ${location}`}
-        src={`https://www.google.com/maps/embed/v1/place?key=${api_key}&q=place_id:${place_id}`}
+        title="Map of Locations"
+        src={`https://www.google.com/maps/embed/v1/search?key=${api_key}&q=${encodeURIComponent(q)}&center=${centerStr}&zoom=15`}
         className="border-0 w-full h-[300px] md:h-[500px]"
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
