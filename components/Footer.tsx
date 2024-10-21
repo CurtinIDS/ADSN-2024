52ae99dcf3412d7721c4ae691b6dc06187d5cb45
Uno Fang <uno.fang.au@gmail.com>
2024-10-21 14:59:42 +0800

    adjust the footer, especially sponsors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -61,7 +61,21 @@ const Footer = () => {
             <div>
               <p className="pb-2 text-lg text-gray-400">Gold Sponsors</p>
               <div className="text-sm flex flex-wrap justify-center md:justify-start space-x-6 md:space-x-8">
-                <p>Coming Soon</p>
+                <Link
+                  href="https://ardc.edu.au/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:opacity-80 transition-opacity duration-300"
+                >
+                  <Image
+                    src="/ADSN-2024/sponsor_logos/ardc_logo.png"
+                    alt="Gold Sponsor Logo"
+                    width={150}
+                    height={70}
+                    className="h-[70px] w-auto"
+                    priority
+                  />
+                </Link>
               </div>
             </div>
 
@@ -98,52 +112,48 @@ const Footer = () => {
         </div>
 
         {/* Footer Bottom Section */}
-        <div className="col-span-1 lg:col-span-3 text-center md:text-left mt-8">
-          <div className="flex flex-col md:flex-row justify-between items-center md:items-start space-y-4 md:space-y-0 md:space-x-8">
-            {/* Legal Information */}
-            <div>
-              <p className="text-sm text-gray-400">&copy; 2024 Australian Data Science Network. All rights reserved.</p>
-            </div>
+        <div className="col-span-1 lg:col-span-4 grid lg:grid-cols-2 lg:gap-8 mt-8">
+          <div className="text-center lg:text-left">
+            <p className="text-sm text-gray-400">&copy; 2024 Australian Data Science Network. All rights reserved.</p>
+          </div>
 
-            {/* Social Media and Contact Links */}
-            <div>
-              <ul className="list-none flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6">
-                <li className="flex items-center space-x-2">
-                  <Image
-                    src="/ADSN-2024/icons/linkedin.png"
-                    alt="LinkedIn Icon"
-                    width={24}
-                    height={24}
-                    className="h-[24px] w-[24px]"
-                    priority
-                  />
-                  <Link
-                    href="https://www.linkedin.com/company/curtin-institute-for-data-science/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-white no-underline hover:underline transition duration-300"
-                  >
-                    LinkedIn
-                  </Link>
-                </li>
-                <li className="flex items-center space-x-2">
-                  <Image
-                    src="/ADSN-2024/icons/email.png"
-                    alt="Email Icon"
-                    width={24}
-                    height={24}
-                    className="h-[24px] w-[24px]"
-                    priority
-                  />
-                  <Link
-                    href="mailto:adsn2024@curtin.edu.au"
-                    className="text-white no-underline hover:underline transition duration-300"
-                  >
-                    Contact Us
-                  </Link>
-                </li>
-              </ul>
-            </div>
+          <div className="text-center lg:text-left">
+            <ul className="list-none flex justify-center lg:justify-start flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6">
+              <li className="flex items-center space-x-2">
+                <Image
+                  src="/ADSN-2024/icons/linkedin.png"
+                  alt="LinkedIn Icon"
+                  width={24}
+                  height={24}
+                  className="h-[24px] w-[24px]"
+                  priority
+                />
+                <Link
+                  href="https://www.linkedin.com/company/curtin-institute-for-data-science/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white no-underline hover:underline transition duration-300"
+                >
+                  LinkedIn
+                </Link>
+              </li>
+              <li className="flex items-center space-x-2">
+                <Image
+                  src="/ADSN-2024/icons/email.png"
+                  alt="Email Icon"
+                  width={24}
+                  height={24}
+                  className="h-[24px] w-[24px]"
+                  priority
+                />
+                <Link
+                  href="mailto:adsn2024@curtin.edu.au"
+                  className="text-white no-underline hover:underline transition duration-300"
+                >
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
